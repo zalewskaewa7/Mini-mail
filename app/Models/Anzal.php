d6036c98a1acfdf43a32e759bee92eed98ae4e14
zalewskaewa7 <52609597+zalewskaewa7@users.noreply.github.com>
2023-10-08 21:52:31 +0200

    Delete Anzal.php

--- app/Models/Anzal.php
+++ app/Models/Anzal.php
@@ -1,14 +1,1 @@
-<?php
 
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Anzal extends Model
-{
-    use HasFactory;
-    protected $fillable=['email', 'message', 'fileNames'];
-    
-   
-}
